4ff62e70f5d0f52e3162544d39140266c5d8e436
crouse12 <crousekimo@yahoo.com.tw>
2019-09-04 22:36:24 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -101,7 +101,7 @@ namespace microbitarduino {
         serial.writeLine("rfidid="+"\\n")
         basic.pause(10)
         let a=serial.readString()
-        a=a.substr(0, a.length - 1)
+        a=a.substr(0, a.length - 2)
         return a
     } 
       
